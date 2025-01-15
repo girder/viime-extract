2246a6ef0869bee6094033a8303f1b0f3618f076
Forrest <forrest.li@kitware.com>
2025-01-15 14:18:10 -0500

    Add script to extract from text file

--- bin/extract_from_pdf.py
+++ bin/extract_from_pdf.py
@@ -5,17 +5,13 @@ from logging import getLogger
 from pathlib import Path
 
 import dotenv
-import langchain_text_splitters
 import tomllib
-from langchain_community import document_loaders
-from langchain_community.document_loaders.base import BaseLoader
 from langchain_openai import ChatOpenAI
-from langchain_text_splitters.base import TextSplitter
-from pydantic import BaseModel, Field, field_validator
 
 sys.path.append(str(Path(__file__).parent / ".."))
 
 from viime_extract.pdf_extract import extract_article_from_document_loader
+from viime_extract.config import Config
 
 logging.basicConfig(level=logging.INFO)
 logger = getLogger(Path(__file__).name)
@@ -28,61 +24,6 @@ class ProgArgs(Namespace):
     pdf_file: Path
     config: Path
     output: str | None
-
-
-class PDFLoaderConfig(BaseModel):
-    class_name: str = "PyPDFLoader"
-
-    @field_validator("class_name", mode="after")
-    @classmethod
-    def is_valid_loader(cls, name: str) -> str:
-        if not hasattr(document_loaders, name):
-            raise ValueError(
-                f"{name} is not a valid langchain_community document loader"
-            )
-        return name
-
-    def create_loader(self, *args, **kwargs) -> BaseLoader:
-        return getattr(document_loaders, self.class_name)(*args, **kwargs)
-
-
-class TextSplitterConfig(BaseModel):
-    class_name: str = "RecursiveCharacterTextSplitter"
-    from_tiktoken_encoder: bool = False
-    chunk_size: int = 1024
-    chunk_overlap: int = 128
-
-    @field_validator("class_name", mode="after")
-    @classmethod
-    def is_valid_loader(cls, name: str) -> str:
-        if not hasattr(langchain_text_splitters, name):
-            raise ValueError(
-                f"{name} is not a valid langchain_text_splitters text splitter"
-            )
-        return name
-
-    def create_splitter(self, *args, **kwargs) -> TextSplitter:
-        klass: TextSplitter = getattr(langchain_text_splitters, self.class_name)
-        if self.from_tiktoken_encoder:
-            return klass.from_tiktoken_encoder(
-                chunk_size=self.chunk_size,
-                chunk_overlap=self.chunk_overlap,
-                *args,
-                **kwargs,
-            )
-        return klass(
-            chunk_size=self.chunk_size,
-            chunk_overlap=self.chunk_overlap,
-            *args,
-            **kwargs,
-        )
-
-
-class Config(BaseModel):
-    model_name: str = "gpt-4o"
-    temperature: float = 0
-    pdf_loader: PDFLoaderConfig = Field(default_factory=PDFLoaderConfig)
-    text_splitter: TextSplitterConfig = Field(default_factory=TextSplitterConfig)
 
 
 def parse_args() -> ProgArgs:
