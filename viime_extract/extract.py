fb3ae6e3850d451bf82c073c670f6820a5341521
Forrest <forrest.li@kitware.com>
2025-03-13 11:19:34 -0400

    Threaded extraction

--- viime_extract/extract.py
+++ viime_extract/extract.py
@@ -1,4 +1,5 @@
 from logging import getLogger
+from concurrent.futures import Executor, ThreadPoolExecutor, as_completed
 
 from langchain_community.document_loaders.base import BaseLoader
 from langchain_core.documents.base import Document
@@ -28,15 +29,47 @@ def extract_article_metadata(
     return model.with_structured_output(ArticleMeta).invoke(llm_input)
 
 
+def extract_article_metadata_with_executor(
+    pages: list[Document], model: BaseChatModel, prompt: Prompt, executor: Executor
+):
+    return executor.submit(extract_article_metadata, pages, model, prompt).result()
+
+
+def extract_article_keywords_from_doc(
+    doc: Document, model: BaseChatModel, prompt: Prompt
+):
+    template = prompt.get_template()
+    llm_input = template.invoke({"article_contents": doc.page_content})
+    return model.with_structured_output(ArticleKeyWords).invoke(llm_input)
+
+
 def extract_article_keywords(
     pages: list[Document], model: BaseChatModel, splitter: TextSplitter, prompt: Prompt
 ):
-    template = prompt.get_template()
     keywords = ArticleKeyWords()
     for text_chunk in splitter.split_documents(pages):
-        llm_input = template.invoke({"article_contents": text_chunk.page_content})
-        response = model.with_structured_output(ArticleKeyWords).invoke(llm_input)
+        response = extract_article_keywords_from_doc(text_chunk, model, prompt)
         keywords = keywords.merge(response)
+
+    # TODO duplicate removal
+    return keywords
+
+
+def extract_article_keywords_with_executor(
+    pages: list[Document],
+    model: BaseChatModel,
+    splitter: TextSplitter,
+    prompt: Prompt,
+    executor: Executor,
+):
+    keywords = ArticleKeyWords()
+    futures = [
+        executor.submit(extract_article_keywords_from_doc, text_chunk, model, prompt)
+        for text_chunk in splitter.split_documents(pages)
+    ]
+
+    for future in as_completed(futures):
+        keywords = keywords.merge(future.result())
 
     # TODO duplicate removal
     return keywords
@@ -52,14 +85,17 @@ def extract_article_from_document_loader(
     article_keywords_prompt: ArticleKeywordsPrompt,
     without_references=False,
 ):
-    pages = list(doc_loader.lazy_load())
-    if without_references:
-        limit = get_start_of_references_idx(pages, model, detect_references_prompt)
-        logging.info("Ignoring pages starting with index %d", limit)
-    else:
-        limit = len(pages)
-    meta = extract_article_metadata(pages[:limit], model, article_metadata_prompt)
-    keywords = extract_article_keywords(
-        pages[:limit], model, splitter, article_keywords_prompt
-    )
-    return Article(meta=meta, keywords=keywords)
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        pages = list(doc_loader.lazy_load())
+        if without_references:
+            limit = get_start_of_references_idx(pages, model, detect_references_prompt)
+            logging.info("Ignoring pages starting with index %d", limit)
+        else:
+            limit = len(pages)
+        meta_future = executor.submit(
+            extract_article_metadata, pages[:limit], model, article_metadata_prompt
+        )
+        keywords = extract_article_keywords_with_executor(
+            pages[:limit], model, splitter, article_keywords_prompt, executor
+        )
+        return Article(meta=meta_future.result(), keywords=keywords)
